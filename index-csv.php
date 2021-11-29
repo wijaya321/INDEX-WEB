d1785783ea4b156c5d43c0ec96b818f1856117ca
wijaya321 <93969270+wijaya321@users.noreply.github.com>
2021-11-29 09:22:35 +0700

    Add files via upload

---
+++ index-csv.php
@@ -0,0 +1,90 @@
+<!--
+-- Source Code from My Notes Code (www.mynotescode.com)
+-- 
+-- Follow Us on Social Media
+-- Facebook : http://facebook.com/mynotescode/
+-- Twitter  : http://twitter.com/mynotescode
+-- Google+  : http://plus.google.com/118319575543333993544
+--
+-- Terimakasih telah mengunjungi blog kami.
+-- Jangan lupa untuk Like dan Share catatan-catatan yang ada di blog kami.
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Halaman Import System Nilai Tutorial Online FK UMS</title>
+
+		<!-- Load File bootstrap.min.css yang ada difolder css -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		  <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		
+		<!-- Style untuk Loading -->
+		<style>
+        #loading{
+			background: whitesmoke;
+			position: absolute;
+			top: 140px;
+			left: 82px;
+			padding: 5px 10px;
+			border: 1px solid #ccc;
+		}
+		</style>
+	</head>
+	<body>
+		<!--
+		-- START HEADER
+		-- Membuat Menu Header / Navbar
+		-- Hapus saja jika tidak diperlukan
+		-->
+		<nav class="navbar navbar-inverse" role="navigation">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="#" style="color: white;"><b>Halaman Import System Nilai Tutorial Online FK UMS</b></a>
+				</div>
+				
+			</div>
+		</nav>
+		<!-- END HEADER -->
+		
+		<!-- Content -->
+		<div style="padding: 0 15px;">
+			<div class="container">
+			<div class="row align-items-center">
+			<div class="col">
+           		<a href="index-quiz.php" class="d-block mb-4 h-100" class="d-block mb-4 h-100">
+                <div class='img-fluid img-thumbnail' style='font-size:40px;color: white;background-color:#4ADB08'>Import Nilai Quiz</div>
+ 				</a>
+        	</div>
+        	
+        	<div class="col">
+           		<a href="index-kelompok.php" class="d-block mb-4 h-100" class="d-block mb-4 h-100">
+                <div class='img-fluid img-thumbnail' style='font-size:40px;color: white;background-color:#041279'>Import Data Kelompok</div>
+ 				</a>
+        	</div>
+
+        	<div class="col">
+           		<a href="index-mahasiswa.php" class="d-block mb-4 h-100" class="d-block mb-4 h-100">
+                <div class='img-fluid img-thumbnail' style='font-size:40px;color: white;background-color:#4ADB08'>Import Mahasiswa</div>
+ 				</a>
+        	</div>
+
+        	<div class="col">
+           		<a href="index-dosen.php" class="d-block mb-4 h-100" class="d-block mb-4 h-100">
+                <div class='img-fluid img-thumbnail' style='font-size:40px;color: white;background-color:#041279'>Import Dosen</div>
+ 				</a>
+        	</div>
+        	</div>
+        </div>
+
+	</body>
+</html>
